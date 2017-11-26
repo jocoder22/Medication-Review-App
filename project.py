c6ddaccc4e26064f8569eb2cfc5047edadec5737
Okechukwu J. Okigbo <okigbookey@gmail.com>
2017-11-26 09:42:18 -0500

    gdisconnect function added on the server(project.py)
     On branch master
     Changes to be committed:
    	modified:   project.py

--- project.py
+++ project.py
@@ -43,6 +43,8 @@ def showLogin():
     #return 'The current session state is %s' % login_session['state']
     return render_template('login.html', STATE=state)
 
+
+# connect the user and initial the OAuth process
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
     # Validate state token
@@ -95,8 +97,7 @@ def gconnect():
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
+        response = make_response(json.dumps('Current user is already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -126,6 +127,37 @@ def gconnect():
     print "done!"
     return output
 
+
+ # DISCONNECT - Revoke a current user's token and reset their login_session
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print 'Access Token is None'
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    print 'In gdisconnect access token is %s', access_token
+    print 'User name is: '
+    print login_session['username']
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is '
+    print result
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 def queryData():
     allCategory = session.query(MedCategory).all()
